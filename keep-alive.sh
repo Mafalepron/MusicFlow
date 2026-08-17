6c98e51a76f7fa338aa4d973eff018130bbeafed
Z User <z@container>
2026-08-17 09:20:14 +0000

    650ccd85-4a63-4789-8ea2-64dc78dfc7a3

--- keep-alive.sh
+++ keep-alive.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/z/my-project
 while true; do
-  node node_modules/.bin/next dev -p 3000 > dev.log 2>&1
+  NODE_OPTIONS="--max-old-space-size=2048" node node_modules/.bin/next dev -p 3000 -H 0.0.0.0 > dev.log 2>&1
+  echo "Server exited, restarting in 1s..." >> dev.log
   sleep 1
 done
